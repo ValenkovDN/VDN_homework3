cee4a28114a7a8d6708799ea566f7f35af10d0f2
Дмитрий <rotweis87@gmail.com>
2022-09-06 20:17:29 +0300

    Добавил решение задачи 23

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+string RasTach()
+{
+Console.Write("Введите число, для составления таблицы ");
+int a = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+string res = "";
+
+while(start <= a)
+{
+    res = res + start * start * start+", ";
+    start = start + 1;
+}
+return res;
+}
+
+try
+    {
+        Console.WriteLine(RasTach());
+    }
+catch
+    {
+        Console.WriteLine("Надо было вводить целое число");
+    }
